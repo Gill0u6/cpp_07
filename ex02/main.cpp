af1c02381d55eef36caca1d0295784c2b6cdd284
Alex Gilles <agilles@c1r5p12.42lehavre.fr>
2025-06-25 13:24:04 +0200

    finish main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: agilles <agilles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 11:01:40 by agilles           #+#    #+#             */
-/*   Updated: 2025/06/25 10:10:42 by agilles          ###   ########.fr       */
+/*   Updated: 2025/06/25 13:23:27 by agilles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 int main(void)
 {
 	{
-		std::cout << "\033[34m" << std::endl << "Empty Array" << "\033[0m" << std::endl;
+		std::cout << "\033[32m" << std::endl << "Empty Array" << "\033[0m" << std::endl;
 		Array<int> emptyArray;
-		std::cout << "\033[34m" << std::endl << "Out Of Bounds" << "\033[0m" << std::endl;
+		std::cout << "\033[32m" << std::endl << "Out Of Bounds" << "\033[0m" << std::endl;
 		try
 		{
 			std::cout << "emptyArray[5]: " << emptyArray[5] << std::endl;
@@ -31,7 +31,7 @@ int main(void)
 
 
 	{
-		std::cout << "\033[34m" << std::endl << "Int Array" << "\033[0m" << std::endl;
+		std::cout << "\033[32m" << std::endl << "Int Array" << "\033[0m" << std::endl;
 		Array<int> intArray(5);
 		for (int i = 0; i < 5; i++)
 			intArray[i] = i;
@@ -40,6 +40,26 @@ int main(void)
 		for (int i = 0; i < 5; i++)
 			std::cout << intArray[i] << " ";
 		std::cout << std::endl;
+
+		std::cout << "\033[32m" << std::endl << "Int Array Copy" << "\033[0m" << std::endl;
+		Array<int> intArrayCopy(intArray);
+		std::cout << "intArrayCopy: ";
+		for (int i = 0; i < 5; i++)
+			std::cout << intArrayCopy[i] << " ";
+		std::cout << std::endl;
+
+	}
+
+	{
+		std::cout << "\033[32m" << std::endl << "Double Array" << "\033[0m" << std::endl;
+	Array<double> doubleArray(10);
+	for (int i = 0; i < 10; i++)
+		doubleArray[i] = i / 2.0;
+	std::cout << "doubleArray size: " << doubleArray.size() << std::endl;
+	std::cout << "doubleArray: ";
+	for (int i = 0; i < 10; i++)
+		std::cout << doubleArray[i] << " ";
+	std::cout << std::endl;
 	}
 	return (0);
 }
